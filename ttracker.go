3d45c34048ece90793abe9416875df6e61cd8358
FritzJo <fritzjo-git@mailbox.org>
2023-07-22 14:32:17 +0200

    fix crash on empty csv file

--- ttracker.go
+++ ttracker.go
@@ -16,26 +16,31 @@ func main() {
 	}
 	recordFileName := strconv.Itoa(time.Now().Year()) + "_data.csv"
 	recordList := m.ReadRecords(recordFileName)
-	switch os.Args[1] {
-	case "in":
-		recordList = m.In(recordList, os.Args)
-	case "out":
-		recordList = m.Out(recordList, os.Args)
-	case "status":
-		fmt.Println(m.Status(recordList))
-	case "summary":
-		recordList = m.Summary(recordList)
-	case "take":
-		recordList = m.Take(recordList)
-	case "update":
-		m.GetVersion()
-	case "validate":
-		err := m.ValidateCSVFile(recordFileName)
-		if err == nil {
-			fmt.Println("OK!")
-		} else {
-			fmt.Println(err)
+	// Check for empty file
+	if len(recordList) > 1 || os.Args[1] == "in" {
+		switch os.Args[1] {
+		case "in":
+			recordList = m.In(recordList, os.Args)
+		case "out":
+			recordList = m.Out(recordList, os.Args)
+		case "status":
+			fmt.Println(m.Status(recordList))
+		case "summary":
+			recordList = m.Summary(recordList)
+		case "take":
+			recordList = m.Take(recordList)
+		case "update":
+			m.GetVersion()
+		case "validate":
+			err := m.ValidateCSVFile(recordFileName)
+			if err == nil {
+				fmt.Println("OK!")
+			} else {
+				fmt.Println(err)
+			}
 		}
+	} else {
+		fmt.Println("Not enough data available for this command. Please clock in at least once.")
 	}
 
 	m.WriteRecords(recordFileName, recordList)
